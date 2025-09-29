4df215968cce76e09763e71e154595dcb22fa86d
Tahir Asadli <tahir-asadov@outlook.com>
2025-09-29 18:59:09 +0400

    Fix featured image block

--- build/default-featured-image/render.php
+++ build/default-featured-image/render.php
@@ -1,4 +1,5 @@
 <?php
+$postId = $block->context['postId'];
 $height = !empty($attributes['height']) ? $attributes['height'] . 'px' : 'initial';
 $imagePosition = !empty($attributes['imagePosition']) ? $attributes['imagePosition'] : 'initial';
 $showFeaturedImage = !empty($attributes['showFeaturedImage']) && $attributes['showFeaturedImage'] == true ? true : false;
@@ -7,10 +8,10 @@ $classes = [];
 $classes[] = 'wp-block-miscellaneous-gutenberg-blocks-default-featured-image--' . $imagePosition;
 $imageUrl = !empty($attributes['imageUrl']) ? $attributes['imageUrl'] : '';
 $imageAlt = !empty($attributes['imageAlt']) ? $attributes['imageAlt'] : '';
-$postThumbnail = get_the_post_thumbnail_url();
-$postLink = get_the_permalink();
-$postTitle = get_the_title();
-if ($postThumbnail) {
+$postThumbnail = get_the_post_thumbnail_url($postId);
+$postLink = get_the_permalink($postId);
+$postTitle = get_the_title($postId);
+if ($showFeaturedImage) {
   $imageUrl = $postThumbnail;
   $imageAlt = 'Post image';
 }
@@ -19,16 +20,13 @@ $styles .= 'height:' . $height . ';';
 $additional_attributes['class'] = join(' ', $classes);
 $additional_attributes['id'] = 'miscellaneous-gutenberg-blocks-' . uniqid();
 $additional_attributes['style'] = $styles;
-
 ?>
 <div <?php echo get_block_wrapper_attributes($additional_attributes); ?>>
-  <?php if (!empty($attributes['imageUrl'])) { ?>
-    <?php if ($isLink) { ?>
-      <a href="<?php echo $postLink; ?>" title="<?php echo $postTitle; ?>">
-      <?php } ?>
-      <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>">
-      <?php if ($isLink) { ?>
-      </a>
+  <?php if ($isLink) { ?>
+    <a href="<?php echo $postLink; ?>" title="<?php echo $postTitle; ?>">
     <?php } ?>
+    <img src="<?php echo $imageUrl; ?>" alt="<?php echo $imageAlt; ?>">
+    <?php if ($isLink) { ?>
+    </a>
   <?php } ?>
 </div>
